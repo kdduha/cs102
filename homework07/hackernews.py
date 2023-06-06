20816d00da8113a31ed9f5c1b9405b4b7e7116d2
Andrew Kuznetsov <just.andrew.kd@gmail.com>
2023-06-06 04:57:45 +0300

    plz

--- homework07/hackernews.py
+++ homework07/hackernews.py
@@ -1,9 +1,9 @@
 import re
 
 import pymorphy2  # type: ignore
-from bottle import redirect, request, route, run, template  # type: ignore
 
 from bayes import NaiveBayesClassifier
+from bottle import redirect, request, route, run, template  # type: ignore
 from db import News, session
 from scraputils import get_news
 
